3149c936c58a95eeb1133d80436e7013d7aebd3f
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2019-04-26 22:49:22 -0400

    Made slight modifications to seir, started extending simulation code

--- seir.py
+++ seir.py
@@ -1,7 +1,9 @@
 import sys
 import random
-import snap
 import time
+
+import snap
+
 #Unsure Aspects: X is a normally distributed random variable, but unsure as to the range. Defined best guess in do_simulation function.
 #WS seems to be the slowest, most aberrant behavior.
 #For the current test, alpha of 0.9 and zeta of 0.1.
@@ -13,7 +15,7 @@ import time
 
 #Assuming undirected graphs:
 class Graph:
-	def __init__(self,alpha,zeta):
+	def __init__(self,alpha,zeta,snap_graph=None):
 		self.verts=set()
 		self.states={}
 		self.edges={}
@@ -22,6 +24,10 @@ class Graph:
 		self.zeta=zeta
 		#Used to store the number of nodes in each state at each timestep.
 		self.values_at_each={}
+		if snap_graph:
+			for it in snap_graph.Edges():
+				self.AddEdge(it.GetSrcNId(),it.GetDstNId())
+
 	def AddNode(self,index):
 		if not index in self.verts:
 			self.verts.add(index)
@@ -34,23 +40,11 @@ class Graph:
 		self.edges[index2].add(index1)
 		self.edgeNums+=1
 	#returns the number of neighbors in each state.
-	def neighborsStates(self,index):
-		neighbors=self.edges[index]
-		numR=0
-		numI=0
-		numE=0
-		numS=0
-		for adj in neighbors:
-			result=self.states[adj]
-			if result[0]==1:
-				numR+=1
-			elif result[1]==1:
-				numI+=1
-			elif result[2]==1:
-				numE+=1
-			else:
-				numS+=1
-		return (numR,numI,numE,numS)	
+	def InfectiousNeighbors(self,index):
+		for adj in self.edges[index]:
+			if self.states[adj][1] == 1:
+				return True
+		return False
 	
 	def do_simulation(self,amount_of_rounds):
 		rounds=0
@@ -58,26 +52,22 @@ class Graph:
 		random.seed(None)
 		current_period={i:-1 for i in self.verts}
 		current_threshold={i:-1 for i in self.verts}
-		#Based on slides assuming incubation period is only 10-12 days, to add more add them into the list.
-		incubation_days=[10,11,12]
-		#Assuming the same for the infection days.
-		infection_days=[5,6]
+
 		while rounds<amount_of_rounds:
 			next_states={}
 			#The amount of nodes in each state. From Left to right for indicies, R:0,I:1,E:2,S:3.
 			amounts={0:0,1:0,2:0,3:0,4:0}
 			for vert in self.verts:
-				randChance=float(random.randint(0,100))/100.0
+				randChance=random.random()
 				#S
 				if self.states[vert]==(0,0,0,1):
-					neighbors=self.neighborsStates(vert)
 					#Chance to transition from S to E.
-					if neighbors[1]>0 and randChance<=self.alpha:
+					if self.InfectiousNeighbors(vert) and randChance<=self.alpha:
 						next_states[vert]=(0,0,1,0)
 						amounts[2]+=1
 						current_period[vert]=0
 						#Generate the incubation time threshold X sub e.
-						current_threshold[vert]=incubation_days[random.randint(0,len(incubation_days)-1)]
+						current_threshold[vert]=random.gauss(11,2)
 					#Else remain in S.
 					else:
 						next_states[vert]=(0,0,0,1)
@@ -85,11 +75,12 @@ class Graph:
 				#E
 				elif self.states[vert]==(0,0,1,0):
 					#check the probability for transition to E.
-					if randChance<=(current_period[vert]/current_threshold[vert]):
+					if current_period[vert] >= current_threshold[vert] and \
+					current_threshold[vert] > 0:
 						current_period[vert]=0
 						next_states[vert]=(0,1,0,0)
 						#Generate the infection time threshold X sub i.
-						current_threshold[vert]=infection_days[random.randint(0,len(infection_days)-1)]
+						current_threshold[vert]=random.gauss(6,1)
 						amounts[1]+=1
 						continue
 					else:
@@ -100,7 +91,9 @@ class Graph:
 				#I
 				elif self.states[vert]==(0,1,0,0):
 					#Chance of death/recovery occuring.
-					if randChance<=self.zeta+(current_period[vert]/current_threshold[vert]):
+					if randChance<=self.zeta or \
+					(current_period[vert] >= current_threshold[vert] and \
+					current_threshold[vert] > 0):
 						next_states[vert]=(1,0,0,0)
 						amounts[0]+=1
 						continue
@@ -116,10 +109,11 @@ class Graph:
 			self.states=next_states
 			self.values_at_each[rounds]=(amounts[3],amounts[2],amounts[1],amounts[0])
 			rounds+=1
-			print rounds,"\tS:",amounts[3],"  E:",amounts[2],"  I:",amounts[1],"  R:",amounts[0]
+			#print rounds,"\tS:",amounts[3],"  E:",amounts[2],"  I:",amounts[1],"  R:",amounts[0]
 			#Break if every node is recovered/dead or stranded.
 			if amounts[0]+amounts[3]==len(self.verts):
 				break
+
 def Loadgraph(filename,alpha,zeta):
 	g=Graph(alpha,zeta)
 	with open(filename,'r') as fp:
@@ -131,113 +125,3 @@ def Loadgraph(filename,alpha,zeta):
 			line=fp.readline()
 	return g
 	
-if len(sys.argv)!=3:
-	print "Improper usage, call as",sys.argv[0]," alpha_value zeta_value\n"
-	sys.exit(1)
-
-
-alpha=float(sys.argv[1])
-zeta=float(sys.argv[2])
-	
-current_seed=6
-#To seed the graph of the same number, push the number to gen then generate the graph.	
-#GenRndGnm for ER graph. GenRndGnm(snap.PNGraph,node_nums,edge_nums,false,TRnd for random number generation).
-#GenSmallWorld for WS graph. GenSmallWorld(node_nums,avgerage_degree,rewire probability,TRnd for random number generation)
-#GenPrefAttach for BA graph. GenPrefAttach(node_nums, NodeOutDegree,TRnd for random number generation)
-
-
-gen=snap.TRnd()
-
-
-ER_Nodes=100000
-ER_Edges=400000
-#Single instance for ER graph.
-gen.PutSeed(current_seed)
-graphER=Graph(alpha,zeta)
-ER=snap.GenRndGnm(snap.PUNGraph,ER_Nodes,ER_Edges,False,gen)
-for it in ER.Edges():
-	graphER.AddEdge(it.GetSrcNId(),it.GetDstNId())
-#Choose random infected.
-infected=random.randint(0,len(graphER.verts))
-graphER.states[infected]=(0,1,0,0)
-print "ER graph:"
-start=time.time()
-graphER.do_simulation(10000)
-end=time.time()
-print "Simulation took",(end-start)
-
-print "Writing to output:"
-with open("EROutput.txt",'w+') as fp:
-	header=str(ER_Nodes)+','+str(ER_Edges)+'\n'
-	fp.write(header)
-	timesteps=graphER.values_at_each
-	for i in range(0,len(timesteps)):
-		line=str(timesteps[i][0])+','+str(timesteps[i][1])+','+str(timesteps[i][2])+','+str(timesteps[i][3])+'\n'
-		fp.write(line)
-print "Finished outputting\n"
-		
-		
-WS_Nodes=50000
-WS_Edges=1000
-#Single instance for WS graph.
-gen.PutSeed(current_seed)
-graphWS=Graph(alpha,zeta)
-WS=snap.GenSmallWorld(WS_Nodes,WS_Edges,0,gen)
-for it in WS.Edges():
-	graphWS.AddEdge(it.GetSrcNId(),it.GetDstNId())
-#Choose random infected.
-infected=random.randint(0,len(graphWS.verts))
-graphWS.states[infected]=(0,1,0,0)
-print "WS graph:"
-start=time.time()
-graphWS.do_simulation(10000)
-end=time.time()
-print "Simulation took",(end-start)
-
-
-print "Writing to output:"
-with open("WSOutput.txt",'w+') as fp:
-	header=str(WS_Nodes)+','+str(WS_Edges)+'\n'
-	fp.write(header)
-	timesteps=graphWS.values_at_each
-	for i in range(0,len(timesteps)):
-		line=str(timesteps[i][0])+','+str(timesteps[i][1])+','+str(timesteps[i][2])+','+str(timesteps[i][3])+'\n'
-		fp.write(line)
-print "Finished outputting\n"
-
-
-
-BA_Nodes=100000
-BA_Edge=10
-#Single instance for WS graph.
-gen.PutSeed(current_seed)
-graphBA=Graph(alpha,zeta)
-BA=snap.GenPrefAttach(BA_Nodes,BA_Edge,gen)
-for it in BA.Edges():
-	graphBA.AddEdge(it.GetSrcNId(),it.GetDstNId())
-#Choose random infected.
-infected=random.randint(0,len(graphBA.verts))
-graphBA.states[infected]=(0,1,0,0)
-print "BA graph:"
-start=time.time()
-graphBA.do_simulation(10000)
-end=time.time()
-print "Simulation took",(end-start)
-
-print "Writing to output:"
-with open("BAOutput.txt",'w+') as fp:
-	header=str(BA_Nodes)+','+str(BA_Edge)+'\n'
-	fp.write(header)
-	timesteps=graphBA.values_at_each
-	for i in range(0,len(timesteps)):
-		line=str(timesteps[i][0])+','+str(timesteps[i][1])+','+str(timesteps[i][2])+','+str(timesteps[i][3])+'\n'
-		fp.write(line)
-print "Finished outputting\n"
-
-
-
-
-
-
-
-
--- simulations.py
+++ simulations.py
@@ -1,9 +1,9 @@
 import sys
 import time
-
-import snap 
-
-import seir_snap
+import random
+ 
+import snap
+import seir
 
 if len(sys.argv)!=3:
 	print "Improper usage, call as",sys.argv[0]," alpha_value zeta_value\n"
@@ -19,23 +19,80 @@ current_seed=6
 #GenSmallWorld for WS graph. GenSmallWorld(node_nums,avgerage_degree,rewire probability,TRnd for random number generation)
 #GenPrefAttach for BA graph. GenPrefAttach(node_nums, NodeOutDegree,TRnd for random number generation)
 
-
 gen=snap.TRnd()
 
+def run_simulation(input_graph,iterations):
+	g = seir.Graph(alpha, zeta, input_graph)
+	#Choose random infected.
+	g.states[random.sample(g.verts,1)[0]] = (0,1,0,0)
+	g.do_simulation(iterations)
+	return g.values_at_each
 
+num_runs = 100
 ER_Nodes=10000
 ER_Edges=40000
 #Single instance for ER graph.
 gen.PutSeed(current_seed)
-graphER=snap.GenRndGnm(snap.PUNGraph,ER_Nodes,ER_Edges,False,gen)
+er_runs = []
+for run in range(0,num_runs):
+	g = snap.GenRndGnm(snap.PUNGraph,ER_Nodes,ER_Edges,False,gen)
+	er_runs.append(run_simulation(g,1000))
+print er_runs
+'''
+WS_Nodes=50000
+WS_Edges=1000
+#Single instance for WS graph.
+gen.PutSeed(current_seed)
+graphWS=seir.Graph(alpha,zeta)
+WS=snap.GenSmallWorld(WS_Nodes,WS_Edges,0,gen)
+for it in WS.Edges():
+	graphWS.AddEdge(it.GetSrcNId(),it.GetDstNId())
 #Choose random infected.
-states = {}#snap.TIntH()
-for node in graphER.Nodes():
-	states[node.GetId()] = 1
-states[graphER.GetRndNId(gen)]=4
-print "ER graph:"
+infected=random.randint(0,len(graphWS.verts))
+graphWS.states[infected]=(0,1,0,0)
+print "WS graph:"
 start=time.time()
-result = seir_snap.do_simulation(graphER,100,states,alpha,zeta)
+graphWS.do_simulation(10000)
 end=time.time()
 print "Simulation took",(end-start)
-print result
+
+
+print "Writing to output:"
+with open("WSOutput.txt",'w+') as fp:
+	header=str(WS_Nodes)+','+str(WS_Edges)+'\n'
+	fp.write(header)
+	timesteps=graphWS.values_at_each
+	for i in range(0,len(timesteps)):
+		line=str(timesteps[i][0])+','+str(timesteps[i][1])+','+str(timesteps[i][2])+','+str(timesteps[i][3])+'\n'
+		fp.write(line)
+print "Finished outputting\n"
+
+
+
+BA_Nodes=100000
+BA_Edge=10
+#Single instance for WS graph.
+gen.PutSeed(current_seed)
+graphBA=seir.Graph(alpha,zeta)
+BA=snap.GenPrefAttach(BA_Nodes,BA_Edge,gen)
+for it in BA.Edges():
+	graphBA.AddEdge(it.GetSrcNId(),it.GetDstNId())
+#Choose random infected.
+infected=random.randint(0,len(graphBA.verts))
+graphBA.states[infected]=(0,1,0,0)
+print "BA graph:"
+start=time.time()
+graphBA.do_simulation(10000)
+end=time.time()
+print "Simulation took",(end-start)
+
+print "Writing to output:"
+with open("BAOutput.txt",'w+') as fp:
+	header=str(BA_Nodes)+','+str(BA_Edge)+'\n'
+	fp.write(header)
+	timesteps=graphBA.values_at_each
+	for i in range(0,len(timesteps)):
+		line=str(timesteps[i][0])+','+str(timesteps[i][1])+','+str(timesteps[i][2])+','+str(timesteps[i][3])+'\n'
+		fp.write(line)
+print "Finished outputting\n"
+'''
